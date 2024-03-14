b68bda9857179da7b0879d14b09192505b535134
essaid bn <essaidcit00@gmail.com>
2024-03-14 09:30:26 +0000

    it fin

--- src/pages/posofsales/index.vue
+++ src/pages/posofsales/index.vue
@@ -6,28 +6,80 @@
     modifier une table     </q-btn>
 
     <q-btn   class=" q-mb-md"  color="primary" icon="mdi-delete-empty" @click="lafonctionq('confirm')">
-    modifier une table     </q-btn>
+    Selectionner un client      </q-btn>
     <q-btn   class=" q-mb-md"  color="primary" icon="mdi-delete-empty" @click="lafonctionq('confirm')">
-    modifier une table     </q-btn>
+    Imprimer une facture    </q-btn>
     <q-btn  class=" q-mb-md"   color="primary" icon="mdi-delete-empty" @click="lafonctionq('confirm')">
-    modifier une table     </q-btn>
+    un lock ticket     </q-btn>
     <q-btn  class=" q-mb-md"   color="primary" icon="mdi-delete-empty" @click="lafonctionq('confirm')">
-    modifier une table     </q-btn>
+    Ajouter un ticket    </q-btn>
     <q-btn  class=" q-mb-md"   color="primary" icon="mdi-delete-empty" @click="lafonctionq('confirm')">
     modifier une table     </q-btn>
 
     </div>
     <div class="col bg-white shadow-2 q-pa-md q-ma-sm">
-kk
+      <q-list padding dense>
+
+
+          <q-item v-for="item in productList" :key="item.pid">
+            <q-item-section avatar clickable>
+              <q-img
+            :src="item.image"
+                color="positive"
+                name="mdi-check"
+              />
+
+            </q-item-section>
+
+            <q-item-section clickable>
+              {{ item.pname }}
+            </q-item-section>
+          </q-item>
+        </q-list>
     </div>
     <div class="col-2 bg-white shadow-2 q-pa-md q-ma-sm">
-      <p  v-for="(categorie, index) in categoryList" :key="categorie.cid" >kk</p>
+
+      <div
+      class="col-lg-2 col-md-2 col-xs-4 text-center bg-white shadow-2 q-mx-sm q-mb-md"
+      v-for="(data, index) in categoryList"
+      :key="index"
+    >
+      <ccshadow>
+        <q-card-section class="bg-blue-1" clickable    @click="handelSelect(data.cid)">
+          <q-img :src="data.image" width="50px" />
+          <q-item-label>{{ data.cname }}</q-item-label>
+        </q-card-section>
+      </ccshadow>
+    </div>
     </div>
     <div class="col bg-green-1 shadow-2 q-pa-md q-ma-sm">
-      <p>kk</p>
-    </div>
+      <div class="row">
+        <div class="col-6 " v-for="item in productList" :key="item.pid">
+          <q-card class="fit bg-cyan-2">
+            <q-card-section>
+              <div class="row">
+                <q-btn
+                  flat
+                  size="xl"
+                  icon="mdi-arrow-left"
+                />
+                <div class="col text-h6 text-center text-weight-bolder">word </div>
+                <q-btn
+                  flat
+                  size="xl"
+                  icon="mdi-arrow-right"
+                />
+              </div>
+            </q-card-section>
 
-  </ q-page>
+          </q-card>
+      </div>
+
+      </div>
+
+
+  </div>
+  </q-page>
 
 
 </template>
@@ -41,7 +93,8 @@ import usecategoriestore from 'stores/modules/category'
 const productList = reactive([])
 const categoryList = reactive([])
 const source2 = ref('');
-
+import ccshadow from 'components/ccshadow.vue';
+const handelSelect = (idProduct) => {}
 
 const addProduct = (idProduct) => {
   productList.push({
